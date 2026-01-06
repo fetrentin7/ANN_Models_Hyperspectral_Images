b825fb96e7d89c181d2a7ba6cc2698dfe7576eff
fetrentin7 <fetrentin7@gmail.com>
2026-01-06 01:11:19 -0300

    reading indian pine dataset

--- readata.py
+++ readata.py
@@ -1,1 +1,11 @@
+import numpy as np
+import pandas as pd
+import os
 
+data = np.load("Datasets/Indian_Pines/indianpinearray.npy")
+
+#print(f"Data type: {data.dtype}")
+print(f"Data shape: {data.shape}")
+
+first_element = data.flat[0]
+print(f"The first element is: {first_element}")
